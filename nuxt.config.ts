6aa0e561087440afbd0852641a0d6a77b0218fcc
MichelCode <michelseocode.com>
2023-12-13 14:50:14 +0100

    v1 pokedex

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  build: {
+    transpile: ["tslib"],
+  },
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/apollo", "@nuxtjs/tailwindcss", "@nuxt/image"],
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: "https://eu-central-1-shared-euc1-02.cdn.hygraph.com/content/clpd3clevd29e01uie0ob3skk/master",
+      },
+    },
+  },
+});
